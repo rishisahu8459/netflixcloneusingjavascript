758752d5ddc6a3f73391c6608ec1d929de9f2e39
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-05-31 01:16:10 +0530

    https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './component/nav/Nav'
 import Header from './header/Header';
 import Content from './content/Content';
 import axios from 'axios';
-axios.defaults.baseURL = 'http:/api.themoviedb.org/3';
+axios.defaults.baseURL = 'https:/api.themoviedb.org/3';
 axios.defaults.params = {
   api_key : '1650be052ad504dcacebe6d51574ee3c',
   };

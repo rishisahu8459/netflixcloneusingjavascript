833af8dd3992a3952284bd75ab77075a097229d1
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-05-31 01:12:53 +0530

    done uii

--- src/component/nav/card/Card.js
+++ src/component/nav/card/Card.js
@@ -5,7 +5,7 @@ import './Card.css'
 
 export default function Card(props) {
 
-  const imageUrl = `https://image.tmdb.org/t/p/original/${props.poster}`;
+  const imageUrl = `http://image.tmdb.org/t/p/original/${props.poster}`;
   console.log(imageUrl);
 
 
